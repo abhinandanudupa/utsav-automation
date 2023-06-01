5aacda63d903da057807eed9a4e7d4e1e5ec4c47
abhinandanudupa <67597710+abhinandanudupa@users.noreply.github.com>
2023-06-01 12:39:44 +0530

    Refractor and fix issues

--- ChatGPT/generators.py
+++ ChatGPT/generators.py
@@ -5,12 +5,17 @@ from abc import ABC, abstractmethod
 import openai
 import traceback
 import time
+from dotenv import dotenv_values
+
+
+config = dotenv_values(".env")
+OPEN_AI_KEY = config["OPEN_AI_KEY"]
+openai.api_key = OPEN_AI_KEY
 
 
 class BaseGenerator(ABC):
     def __init__(self, event_list, openai_api_key=None):
         self.event_list = event_list
-        openai.api_key = openai_api_key
         self.all_prompts = []
         self.all_replies = []
         self.all_outputs = []
@@ -26,6 +31,7 @@ class BaseGenerator(ABC):
         #         "content": prompt
         #     }]
         # )
+        # print("Sleeping for 20s...")
         # time.sleep(20)
         # return completion.choices[0].message.content
         return F"Reply for {prompt}"
@@ -170,7 +176,9 @@ class InvitationGenerator(BaseGenerator, ABC):
         event_name = event['eventName']
         description = event['description']
         rules = event['rules']
-        role = 'Please note that he/she is the judge for the event'
+        venue = event['venue']
+        date_and_timings = event['eventDate']
+        mode = event['eventMode']
         event_id = event['eventId'].strip()
 
         prompts = []
@@ -178,6 +186,12 @@ class InvitationGenerator(BaseGenerator, ABC):
             prompt = F"""
 With this description of the competition below:
 Event Name: {event_name}
+
+Mode: {mode}
+
+Date and Timings: {date_and_timings}
+
+Venue: {venue}
 
 Description:
 {description}
@@ -187,8 +201,25 @@ Rules:
 
 Resource Person:
 {judge['name']}
+
 Experience:
 {judge['brief_description']}
+
+And this description of UTSAV 2023:
+
+Utsav - the Festival of Faith - is a grand cultural fest hosted by B M S College of Engineering in Bengaluru. Over the years, Utsav has established itself as a brand, and has become one of the biggest and most popular cultural fests hosted by any college in Bengaluru, providing an invaluable platform to explore one's talents and artistic endeavours in areas such as music, dance, literature, theatre, and more.
+
+Passionate students are given ample opportunities to express themselves and compete with one another, with creativity, courage, and persistence being rewarded generously at Utsav. Drawing participants from all over India, the festival has set new benchmarks with its events on numerous occasions, making it an unforgettable experience for years to come.
+
+Utsav is a landmark celebration, and has been admired for its dedication to beat with the rhythms of cosmopolitan Bengaluru. Standing tall since 1981, this mega-event has earned a distinguished reputation in South India for not only being a thriving hub of cultural excellence but also for warmly embracing everyone, regardless of any barriers. Coming at no cost, this student fest draws in crowds like moths to flame.
+
+This year, with the theme of "Regalia", the bar can only be set higher, and we eagerly await your participation in this extraordinary cultural celebration.
+
+Theme:
+Regalia emulates royalty - it is not just an emotion, but a way of life that makes one feel a sense of belonging while also being in an exalted state of being. Regality is experienced by those who give their all, stand firm for what they believe in, and have faith in their purpose, like flowers that bloom through concrete. It does not require material possessions or money, but can be found in one's passion, vigour, and drive to achieve their dreams. When someone is driven by a strong sense of purpose and a burning desire to succeed, they radiate a regal aura that commands respect, admiration and reverence. In essence, true royalty is not about what one possesses, but rather about who they are and what they stand for.
+
+Utsav 2023 aims to uplift and nurture the light within us by celebrating our unique talents, strengths, and individuality. Fondly and aptly known as the Festival of Faith, Utsav is spotlighting the indomitable human spirit in its 2023 edition. 
+
 
 You are a an invitation letter writing AI. You task is to write emails to judges inviting them to the event mentioned above which is a part of a college cultural fest.
 
@@ -248,10 +279,14 @@ The event will be conducted in {mode_of_conduction} mode from {timings} at {venu
         if save_prompts:
             events_n_prompts = zip(self.event_list, self.all_prompts)
             for i, (event, prompts) in enumerate(events_n_prompts):
-                for prompt in prompts:
+                for prompt, rp_details in zip(prompts, self.rp_details.get(event['eventId'], [])):
+                    rp_name = rp_details['name']
+                    rp_email = rp_details['email_id']
+                    desc = rp_details['brief_description']
                     data = deepcopy(self.event_relevant_data[i])
                     data.update({
-                        "reply": prompt
+                        "prompt": prompt,
+                        "email": rp_email
                     })
                     prompt_data.append(data)
 
